80f78e6544f35b2475ce1f70d41962539e6e4d58
Clark Perkins <clark.perkins@digitalreasoning.com>
2015-04-28 17:05:06 -0500

    Fixed issue with creating directories

--- elasticsearch/create_mnt_dirs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -uef -o pipefail
-
-mkdir /mnt/elasticsearch
-chown elasticsearch:elasticsearch /mnt/elasticsearch
-sudo -u elasticsearch mkdir -p /mnt/elasticsearch/{data,work,logs}
